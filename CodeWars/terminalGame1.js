94c9d3836f3392e9e1b3e0ae843e158b7834b7fe
codimario <codimorio@gmail.com>
2023-02-11 18:50:38 +0300

    init: create terminal-game-1 kata

---
+++ CodeWars/terminalGame1.js
@@ -0,0 +1,15 @@
+function Hero (name) {
+  this.name = name || 'Hero'
+  this.experience = 0
+  this.health = 100
+  this.position = '00'
+  this.damage = 5
+}
+
+/*
+Grasshopper - Terminal Game #1
+
+You have chosen to translate this kata. For your convenience we have provided the existing test cases used for the language that you have already completed, as well as all of the other related fields.
+
+View the help tab for more information on how kata translations work.
+*/
